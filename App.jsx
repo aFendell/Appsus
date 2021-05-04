63204bb5991a447b076022d6dcfe3f647215fe77
DanaOz78 <dana.ozeri78@gmail.com>
2021-05-04 12:14:52 +0300

    reorder files-dana

--- App.jsx
+++ App.jsx
@@ -4,32 +4,28 @@ import { EmailApp } from './apps/Email/pages/EmailApp.jsx'
 import { KeepApp } from './apps/Keep/pages/KeepApp.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './pages/About.jsx'
-
-
 import { Home } from './pages/Home.jsx'
-
-
 
 export function App() {
     return (
+        <section className="main-container">
         <Router>
             <header>
-                <AppHeader/>
+                <AppHeader />
             </header>
             <main>
                 <Switch>
-                    {/* <Route component={BookDetails} path="/book/:bookId" /> */}
-                    {/* <Route component={BooksApp} path="/book" /> */}
                     <Route component={EmailApp} path="/mail" />
                     <Route component={KeepApp} path="/keep" />
                     <Route component={About} path="/about" />
-                    <Route component={Home} path="/"/>
+                    <Route component={Home} path="/" />
                 </Switch>
             </main>
             <footer>
-               Dana and aFendell &copy;
+                Dana and aFendell &copy;
             </footer>
         </Router>
+        </section>
     )
 }
 
